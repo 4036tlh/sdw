7e648c8982041f6656aa0180e0b1ac365cdcb409
4036tlh <4036tlh@gmail.com>
2023-11-08 11:03:33 +0800

    webui_api_sepearte_ver_1

--- webui_ui_only.py
+++ webui_ui_only.py
@@ -6,11 +6,7 @@ import signal
 import re
 import warnings
 import json
-from threading import Thread
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
 from packaging import version
 
 import logging
@@ -28,11 +24,15 @@ warnings.filterwarnings(action="ignore", category=UserWarning, module="torchvisi
 
 startup_timer.record("import torch")
 
+import gradio
+startup_timer.record("import gradio")
+
 import ldm.modules.encoders.modules
 startup_timer.record("import ldm")
 
 from modules import extra_networks, ui_extra_networks_checkpoints
 from modules import extra_networks_hypernet, ui_extra_networks_hypernets, ui_extra_networks_textual_inversion
+from modules.call_queue import wrap_queued_call, queue_lock, wrap_gradio_gpu_call
 
 # Truncate version number of nightly/local build of PyTorch to not cause exceptions with CodeFormer or Safetensors
 if ".dev" in torch.__version__ or "+git" in torch.__version__:
@@ -263,20 +263,142 @@ def wait_on_server(demo=None):
             modules.script_callbacks.app_reload_callback()
             break
 
-
-def api_only():
+def webui():
+    launch_api = cmd_opts.api
     initialize()
 
-    app = FastAPI()
-    setup_middleware(app)
-    api = create_api(app)
+    while 1:
+        if shared.opts.clean_temp_dir_at_start:
+            ui_tempdir.cleanup_tmpdr()
+            startup_timer.record("cleanup temp dir")
 
-    modules.script_callbacks.app_started_callback(None, app)
+        modules.script_callbacks.before_ui_callback()
+        startup_timer.record("scripts before_ui_callback")
 
-    print(f"Startup time: {startup_timer.summary()}.")
-    api.launch(server_name="0.0.0.0" if cmd_opts.listen else "127.0.0.1", port=cmd_opts.port if cmd_opts.port else 7861)
+        shared.demo = modules.ui.create_ui()
+        startup_timer.record("create ui")
+
+        if not cmd_opts.no_gradio_queue:
+            shared.demo.queue(64)
+
+        gradio_auth_creds = []
+        if cmd_opts.gradio_auth:
+            gradio_auth_creds += [x.strip() for x in cmd_opts.gradio_auth.strip('"').replace('\n', '').split(',') if x.strip()]
+        if cmd_opts.gradio_auth_path:
+            with open(cmd_opts.gradio_auth_path, 'r', encoding="utf8") as file:
+                for line in file.readlines():
+                    gradio_auth_creds += [x.strip() for x in line.split(',') if x.strip()]
+
+        # this restores the missing /docs endpoint
+        if launch_api and not hasattr(FastAPI, 'original_setup'):
+            def fastapi_setup(self):
+                self.docs_url = "/docs"
+                self.redoc_url = "/redoc"
+                self.original_setup()
+
+            FastAPI.original_setup = FastAPI.setup
+            FastAPI.setup = fastapi_setup
+
+        app, local_url, share_url = shared.demo.launch(
+            share=cmd_opts.share,
+            server_name=server_name,
+            server_port=cmd_opts.port,
+            ssl_keyfile=cmd_opts.tls_keyfile,
+            ssl_certfile=cmd_opts.tls_certfile,
+            ssl_verify=cmd_opts.disable_tls_verify,
+            debug=cmd_opts.gradio_debug,
+            auth=[tuple(cred.split(':')) for cred in gradio_auth_creds] if gradio_auth_creds else None,
+            inbrowser=cmd_opts.autolaunch,
+            prevent_thread_lock=True
+        )
+        # after initial launch, disable --autolaunch for subsequent restarts
+        cmd_opts.autolaunch = False
+
+        startup_timer.record("gradio launch")
+
+        # gradio uses a very open CORS policy via app.user_middleware, which makes it possible for
+        # an attacker to trick the user into opening a malicious HTML page, which makes a request to the
+        # running web ui and do whatever the attacker wants, including installing an extension and
+        # running its code. We disable this here. Suggested by RyotaK.
+        app.user_middleware = [x for x in app.user_middleware if x.cls.__name__ != 'CORSMiddleware']
+
+        setup_middleware(app)
+
+        modules.progress.setup_progress_api(app)
+        modules.ui.setup_ui_api(app)
+
+        if launch_api:
+            create_api(app)
+
+        ui_extra_networks.add_pages_to_demo(app)
+
+        modules.script_callbacks.app_started_callback(shared.demo, app)
+        startup_timer.record("scripts app_started_callback")
+
+        print(f"Startup time: {startup_timer.summary()}.")
+
+        if cmd_opts.subpath:
+            redirector = FastAPI()
+            redirector.get("/")
+            mounted_app = gradio.mount_gradio_app(redirector, shared.demo, path=f"/{cmd_opts.subpath}")
+
+        wait_on_server(shared.demo)
+        print('Restarting UI...')
+
+        startup_timer.reset()
+
+        sd_samplers.set_samplers()
+
+        modules.script_callbacks.script_unloaded_callback()
+        extensions.list_extensions()
+        startup_timer.record("list extensions")
+
+        config_state_file = shared.opts.restore_config_state_file
+        shared.opts.restore_config_state_file = ""
+        shared.opts.save(shared.config_filename)
+
+        if os.path.isfile(config_state_file):
+            print(f"*** About to restore extension state from file: {config_state_file}")
+            with open(config_state_file, "r", encoding="utf-8") as f:
+                config_state = json.load(f)
+                config_states.restore_extension_config(config_state)
+            startup_timer.record("restore extension config")
+        elif config_state_file:
+            print(f"!!! Config state backup not found: {config_state_file}")
+
+        localization.list_localizations(cmd_opts.localizations_dir)
+
+        modules.scripts.reload_scripts()
+        startup_timer.record("load scripts")
+
+        modules.script_callbacks.model_loaded_callback(shared.sd_model)
+        startup_timer.record("model loaded callback")
+
+        modelloader.load_upscalers()
+        startup_timer.record("load upscalers")
+
+        for module in [module for name, module in sys.modules.items() if name.startswith("modules.ui")]:
+            importlib.reload(module)
+        startup_timer.record("reload script modules")
+
+        modules.sd_models.list_models()
+        startup_timer.record("list SD models")
+
+        shared.reload_hypernetworks()
+        startup_timer.record("reload hypernetworks")
+
+        ui_extra_networks.intialize()
+        ui_extra_networks.register_page(ui_extra_networks_textual_inversion.ExtraNetworksPageTextualInversion())
+        ui_extra_networks.register_page(ui_extra_networks_hypernets.ExtraNetworksPageHypernetworks())
+        ui_extra_networks.register_page(ui_extra_networks_checkpoints.ExtraNetworksPageCheckpoints())
+
+        extra_networks.initialize()
+        extra_networks.register_extra_network(extra_networks_hypernet.ExtraNetworkHypernet())
+        startup_timer.record("initialize extra networks")
 
 
 if __name__ == "__main__":
-    api_only()
-    
+    if cmd_opts.nowebui:
+        api_only()
+    else:
+        webui()
